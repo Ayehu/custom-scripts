ab317529a92dfca7905f7432bf13e0ac02fc4e08
Derek Pascarella <contact.pascarella@gmail.com>
2019-08-22 10:06:32 -0400

    Update workflow_activity_counter.ps1

--- workflow_activity_counter.ps1
+++ workflow_activity_counter.ps1
@@ -8,6 +8,17 @@
 # PowerShell prompt, this script is executed as follows:
 # > C:\full\path\to\workflow_activity_counter.ps1 C:\full\path\to\WorkFlow.xml
 #
+# For Windows users, note that a shortcut can be created with the path "powershell -file C:\full\path\to\workflow_activity_counter.ps1",
+# after which WorkFlow XML files can be dragged from the file explorer directly onto the shortcut.  This would launch a command prompt
+# window and display the activity count results.
+#
+# Windows users may also create a shortcut in their local "SendTo" folder (foud at "C:\Users\<user>\AppData\Roaming\Microsoft\Windows
+# \SendTo") which contains a target path of "powershell -file C:\full\path\to\workflow_activity_counter.ps1".  Once created, one can
+# right-click on a WorkFlow XML file, navigate to the "Send To" menu and select the newly created shortcut.  This would launch a command
+# prompt window and display the activity count results.
+#
+# If utilizing either of these shortcut methods, simply uncomment the last line of this script.
+#
 # Sample output:
 # Total activities: 4
 #
@@ -23,3 +34,7 @@ $workflow_file = $args[0]
 $count = (Get-Content "$workflow_file" | Select-String -Pattern '; id=&quot;' -AllMatches).matches.count
 
 Write-Host "Total activities: $count" -nonewline
+
+# Uncomment the next line if using the shortcut method on Windows to ensure command prompt doesn't disappear after this script has
+# finished executing.
+#$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
