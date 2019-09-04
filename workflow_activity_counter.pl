c374a37a60ffc6127fd13338d17d38d4aa0dbe38
Derek Pascarella <contact.pascarella@gmail.com>
2019-09-04 11:10:37 -0400

    Update workflow_activity_counter.pl

--- workflow_activity_counter.pl
+++ workflow_activity_counter.pl
@@ -6,26 +6,26 @@
 # Derek Pascarella <derekp@ayehu.com>
 # Ayehu, Inc.
 #
-# Count the number of activities in a given WorkFlow XML file.  The full file path should be passed to this script as an argument.
+# Count the number of activities in a given Workflow XML file.  The full file path should be passed to this script as an argument.
 #
 # This script can be executed on Windows using the Strawberry Perl distribution (http://strawberryperl.com/) and executed at the
 # Windows command prompt as follows:
-# > perl C:\full\path\to\workflow_activity_counter.pl C:\full\path\to\WorkFlow.xml
+# > perl C:\full\path\to\workflow_activity_counter.pl C:\full\path\to\Workflow.xml
 #
 # For Windows users, note that a shortcut can be created with the path "perl C:\full\path\to\workflow_activity_counter.pl", after which
-# WorkFlow XML files can be dragged from the file explorer directly onto the shortcut.  This would launch a command prompt window and
+# Workflow XML files can be dragged from the file explorer directly onto the shortcut.  This would launch a command prompt window and
 # display the activity count results.
 #
 # Windows users may also create a shortcut in their local "SendTo" folder (foud at "C:\Users\<user>\AppData\Roaming\Microsoft\Windows
 # \SendTo") which contains a target path of "perl C:\full\path\to\workflow_activity_counter.pl".  Once created, one can right-click
-# on a WorkFlow XML file, navigate to the "Send To" menu and select the newly created shortcut.  This would launch a command prompt
+# on a Workflow XML file, navigate to the "Send To" menu and select the newly created shortcut.  This would launch a command prompt
 # window and display the activity count results.
 #
 # If utilizing either of these shortcut methods, simply uncomment the last line of this script.
 #
 # Sample output:
-#    WorkFlow file: C:\SSH - Linux System Administration\SSH - Linux - Service Status.xml
-#    WorkFlow name: SSH - Linux - Service Status
+#    Workflow file: C:\SSH - Linux System Administration\SSH - Linux - Service Status.xml
+#    Workflow name: SSH - Linux - Service Status
 # Total activities: 4
 
 # Our modules.
@@ -38,10 +38,10 @@ my $workflow_file = $ARGV[0];
 my $workflow_xml;
 my $workflow_name;
 
-# Open WorkFlow XML file or print error if not found.
+# Open Workflow XML file or print error if not found.
 open(FH, '<', $workflow_file) or die $!;
 
-# Iterate through each line of the file until the WorkFlow XML is found.
+# Iterate through each line of the file until the Workflow XML is found.
 while(<FH>)
 {
 	if(grep/\<WorkflowInfo/, $_)
@@ -51,18 +51,21 @@ while(<FH>)
 	}
 }
 
-# Extract the WorkFlow's name from the XML.
+# Close Workflow XML file.
+close(FH);
+
+# Extract the Workflow's name from the XML.
 $workflow_name = $workflow_xml;
 $workflow_name =~ s/^[^Name=\"]*Name=\"//;
 $workflow_name =~ s/\" Description.*//;
 $workflow_name =~ s/^\s+|\s+$//g;
 
-# Count the number of instances "pattern" is found in the WorkFlow XML.
+# Count the number of instances "pattern" is found in the Workflow XML.
 $count = () = $workflow_xml =~ /$pattern/gi;
 
 # Print results.
-print "   WorkFlow file: $workflow_file\n";
-print "   WorkFlow name: $workflow_name\n";
+print "   Workflow file: $workflow_file\n";
+print "   Workflow name: $workflow_name\n";
 print "Total activities: $count\n";
 
 # Uncomment the next line if using the shortcut method on Windows to ensure command prompt doesn't disappear after this script has
