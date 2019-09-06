203a763e50c88846d7e57cae57a31fb6a49f990a
Derek Pascarella <contact.pascarella@gmail.com>
2019-09-05 20:20:51 -0400

    Added backwards compatibility for EyeShare workflows.

--- workflow_activity_counter.pl
+++ workflow_activity_counter.pl
@@ -6,25 +6,31 @@
 # Derek Pascarella <derekp@ayehu.com>
 # Ayehu, Inc.
 #
-# Count the number of activities in a given Workflow XML file.  The full file path should be passed to this script as an argument.
+# Count the number of activities in a given Workflow XML file.  The full file
+# path should be passed to this script as an argument.
 #
-# This script can be executed on Windows using the Strawberry Perl distribution (http://strawberryperl.com/) and executed at the
-# Windows command prompt as follows:
-# > perl C:\full\path\to\workflow_activity_counter.pl C:\full\path\to\Workflow.xml
+# This script can be executed on Windows using the Strawberry Perl distribution
+# (http://strawberryperl.com/) and executed at the Windows command prompt as
+# follows:
+# > perl C:\folder\workflow_activity_counter.pl C:\folder\Workflow.xml
 #
-# For Windows users, note that a shortcut can be created with the path "perl C:\full\path\to\workflow_activity_counter.pl", after which
-# Workflow XML files can be dragged from the file explorer directly onto the shortcut.  This would launch a command prompt window and
-# display the activity count results.
+# For Windows users, note that a shortcut can be created with the path
+# "perl C:\folder\workflow_activity_counter.pl", after which Workflow XML files
+# can be dragged from the file explorer directly onto the shortcut.  This would
+# launch a command prompt window and display the activity count results.
 #
-# Windows users may also create a shortcut in their local "SendTo" folder (foud at "C:\Users\<user>\AppData\Roaming\Microsoft\Windows
-# \SendTo") which contains a target path of "perl C:\full\path\to\workflow_activity_counter.pl".  Once created, one can right-click
-# on a Workflow XML file, navigate to the "Send To" menu and select the newly created shortcut.  This would launch a command prompt
-# window and display the activity count results.
+# Windows users may also create a shortcut in their local "SendTo" folder (foud
+# at "C:\Users\<user>\AppData\Roaming\Microsoft\Windows\SendTo") which contains
+# a target path of "perl C:\full\path\to\workflow_activity_counter.pl".  Once
+# created, one can right-click on a Workflow XML file, navigate to the
+# "Send To" menu and select the newly created shortcut.  This would launch a
+# command prompt window and display the activity count results.
 #
-# If utilizing either of these shortcut methods, simply uncomment the last line of this script.
+# If utilizing either of these shortcut methods, simply uncomment the last line
+# of this script.
 #
 # Sample output:
-#    Workflow file: C:\SSH - Linux System Administration\SSH - Linux - Service Status.xml
+#    Workflow file: C:\Workflows\SSH - Linux - Service Status.xml
 #    Workflow name: SSH - Linux - Service Status
 # Total activities: 4
 
@@ -59,7 +65,7 @@ $workflow_name =~ s/^[^Name=\"]*Name=\"//;
 $workflow_name =~ s/\" Description.*//;
 $workflow_name =~ s/^\s+|\s+$//g;
 
-# Count the number of instances "pattern" is found in the Workflow XML.
+# Count the number of instances "x:Name=&quot;" is found in the Workflow XML.
 $count = (() = $workflow_xml =~ /x:Name=&quot;/gi) - 1;
 
 # Print results.
@@ -67,6 +73,6 @@ print "   Workflow file: $workflow_file\n";
 print "   Workflow name: $workflow_name\n";
 print "Total activities: $count\n";
 
-# Uncomment the next line if using the shortcut method on Windows to ensure command prompt doesn't disappear after this script has
-# finished executing.
+# Uncomment the next line if using the shortcut method on Windows to ensure
+# command prompt doesn't disappear after this script has finished executing.
 #<STDIN>;
