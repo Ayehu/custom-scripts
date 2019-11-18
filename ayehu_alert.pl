1a48b9eb9297369f10408a86d001576186daaec1
Derek Pascarella <contact.pascarella@gmail.com>
2019-11-18 15:56:50 -0500

    Added support for GET/POST requests, passing SIDs, config file

--- ayehu_alert.pl
+++ ayehu_alert.pl
@@ -1,106 +1,152 @@
 #!/usr/bin/perl
 #
 # ayehu_alert.pl
-# Usage: ayehu_alert.pl alertKey1 "alertValue1" alertKey2 "alertValue2"
+# Usage: ayehu_alert --host <LABEL> --mode <GET/POST> alertKey1 "alert value 1" alertKey2 "alert value 2"
 #
 # Derek Pascarella <derekp@ayehu.com>
 # Ayehu, Inc.
-#
-# Send data to Ayehu NG via the Web Service API.  Modify the "url" and
-# "secret" variables to match your Web Service module's configuration.  This
-# script is a useful way to send an HTTP POST request to an Ayehu NG server
-# with a convenient wrapper that's executable at the command-line in a Linux
-# or UNIX environment.
-#
-# The only requirements are the "JSON" and "HTTP::Tiny" Perl libraries.  This
-# script can also be used on Windows using the Strawberry Perl distribution
-# (http://strawberryperl.com/), which already ships with the required
-# libraries, and can be executed at the Windows command prompt as follows:
-# > perl C:\folder\ayehu_alert.pl "alertValue1" alertKey2 "alertValue2"
-#
-# Sample output (success):
-# Status:         Success
-# Session ID:     80efa8eb-320b-415d-be6d-2f7dca18a84c
-# Payload:        {"root":{"item":{"auth":"my_secret_password","name":"Derek","location":"NYC","sessionid":"0"}}}
-#
-# Sample output (failure, incorrect endpoint URL given):
-# Status: Failure
-# Reason: Temporary Redirect
-#
-# Sample output (failure, incorrect secret phrase given):
-# Status: Failure
-# Reason: Unauthorized
 
 # Use strict policy on syntax and data-types.
 use strict;
 
 # Our modules.
 use HTTP::Tiny;
+use Getopt::Long;
 use JSON;
 
+# Define usage help.
+my $usage = "Usage: ayehu_alert --host <LABEL> --mode <GET/POST> alertKey1 \"alert value 1\" alertKey2 \"alert value 2\"\n";
+
+# Define location of configuration file.
+my $config_file = "/etc/ayehu.conf";
+
 # Our variables.
-my $url = "http://localhost:8200/AyehuAPI/";
-my $secret = "my_secret_password";
+my $url;
+my $secret;
+my %hosts;
 my %key_value;
+my @host_info;
+my $post_data;
+my $http;
+my $response;
 my $i;
 
-# If at least one key-value pair wasn't given, display an error.
-if(scalar(@ARGV) < 2)
+# Our arguments.
+my $sid = "0";
+my $mode;
+my $host;
+
+# Define our parameters and arguments.
+GetOptions(
+	'host=s' => \$host,
+	'mode=s' => \$mode,
+	'sid=s' => \$sid
+) or die $usage;
+
+# Convert "mode" and "sid" to lowercase.
+$mode = lc($mode);
+$sid = lc($sid);
+
+# Print usage and exit if insufficient arguments are given.
+if(scalar(@ARGV) % 2 != 0 || $host eq "" || ($mode ne "post" && $mode ne "get"))
 {
-	print "At least one key-value pair must be given.\n";
-	print "Usage: ayehu_alert.pl alertKey1 \"alertValue1\" alertKey2 \"alertValue2\"\n";
+	print $usage;
 
 	exit;
 }
-# If an uneven number of arguments were given, display an error.
-elsif(scalar(@ARGV) % 2 != 0)
-{
-	print "One or more key(s) have been given without a corresponding value.\n";
-	print "Usage: ayehu_alert.pl alertKey1 \"alertValue1\" alertKey2 \"alertValue2\"\n";
 
-	exit;
-}
-# Otherwise, proceed in adding each key-value pair to our hash.
-else
+# Open configuration file.
+open(FH, '<', $config_file) or die $!;
+
+# Read and store information on each host from configuration file.
+while(<FH>)
 {
-	for($i = 0; $i <= $#ARGV; $i += 2)
+	# Skip blank lines and comments.
+	next if /^$/;
+	next if $_ =~ /^#/;
+
+	# Store each property of the host into a separate element of "host_info".
+	chomp;
+	@host_info = split(/\|/, $_);
+
+	# If three properties are present, store them in "hosts" hash.
+	if(scalar(@host_info) == 3)
 	{
-		$key_value{$ARGV[$i]} = $ARGV[$i + 1];
+		$hosts{$host_info[0]}{url} = $host_info[1];
+		$hosts{$host_info[0]}{secret} = $host_info[2];
 	}
 }
 
-# Encode JSON payload.
-my $data = encode_json {
-	root => {
-		item => {
-			auth => $secret,
-			sessionid => 0,
-			%key_value
-		}
-	}
-};
+# Close configuration file.
+close(FH);
+
+# Add each key-value pair from arguments to "key_value" hash.
+for($i = 0; $i <= $#ARGV; $i += 2)
+{
+	$key_value{$ARGV[$i]} = $ARGV[$i + 1];
+}
 
 # Create new "HTTP::Tiny" object.
-my $http = HTTP::Tiny->new;
+$http = HTTP::Tiny->new;
 
-# Send POST request via "http" object.
-my $response = $http->post(
-	$url => {
-		content => $data,
-		headers => { 'Content-Type' => 'application/json' }
+# Send "POST" request per value of "mode" parameter.
+if($mode eq "post")
+{
+	# Encode JSON payload.
+	$post_data = encode_json {
+		root => {
+			item => {
+				auth => $hosts{$host}{secret},
+				sessionid => $sid,
+				%key_value
+			}
+		}
+	};
+
+	# Send "POST" request via "http" object.
+	$response = $http->post(
+		$hosts{$host}{url} => {
+			content => $post_data,
+			headers => { 'Content-Type' => 'application/json' }
+		}
+	);
+
+	# Display successful results.
+	if($response->{'success'} == 1)
+	{
+		print "Status:\t\tSuccess\n";
+		print "Session ID:\t" . decode_json($response->{'content'})->{'SessionID'} . "\n";
+		print "Payload:\t$post_data\n";
 	}
-);
-
-# Display successful results.
-if($response->{'success'} == 1)
-{
-	print "Status:\t\tSuccess\n";
-	print "Session ID:\t" . decode_json($response->{'content'})->{'SessionID'} . "\n";
-	print "Payload:\t$data\n";
+	# Display unsuccessful results.
+	else
+	{
+		print "Status:\tFailure\n";
+		print "Reason:\t" . $response->{'reason'} . "\n";
+	}
 }
-# Display unsuccessful results.
-else
+# Send "GET" request per value of "mode" parameter.
+elsif($mode eq "get")
 {
-	print "Status:\tFailure\n";
-	print "Reason:\t" . $response->{'reason'} . "\n";
+	# Send "GET" request via "http" object.
+	$response = $http->get(
+		$hosts{$host}{url} . "?query=" . $sid => {
+			headers => {
+				'auth' => $hosts{$host}{secret}
+			}
+		}
+	);
+
+	# Display successful results.
+	if($response->{'success'} == 1)
+	{
+		print "Status:\t\tSuccess\n";
+		print "Response:\t" . decode_json($response->{'content'})->{'Response'} . "\n";
+	}
+	# Display unsuccessful results.
+	else
+	{
+		print "Status:\tFailure\n";
+		print "Reason:\t" . $response->{'reason'} . "\n";
+	}
 }
